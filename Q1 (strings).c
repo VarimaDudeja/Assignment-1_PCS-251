afccae912566a74a41bf542325d9ca6fd3f39a44
VarimaDudeja <161865515+VarimaDudeja@users.noreply.github.com>
2024-03-04 23:36:02 +0530

    Add files via upload

---
+++ Q1 (strings).c
@@ -0,0 +1,28 @@
+// Q1. WAP in C to print the frequency of each word of a string.
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char s[100];
+    int f[26] = {0}, i ; 
+    printf("Enter a string: ");
+    gets(s);
+    for (i = 0; s[i]!='\0'; i++) 
+    {
+        if((s[i]>='a'&&s[i]<='z'))
+            f[s[i]-'a']++;
+        
+        else if((s[i]>='A'&&s[i]<='Z')) 
+            f[s[i]-'A']++;
+            
+        else
+            continue;
+    }
+    printf("Frequency of each letter:\n");
+    for (i = 0; i<26; i++) 
+    {
+        if (f[i]>0) 
+            printf("%c: %i\n", 'a'+i, f[i]);
+        
+    }
+}  
---
+++ Q2 (strings).c
@@ -0,0 +1,37 @@
+// Q2. WAP in C to check whether a substring is present in the main string or not.
+#include<stdio.h>
+#include<string.h>
+void main() 
+{
+    char s[100], w[10], c[10];
+    int i, j, x = 0, k = 0;
+    printf("Enter a string: ");
+    gets(s);
+    printf("Enter the substring to check: ");
+    gets(w);
+    for (i = 0; s[i] != '\0'; i++) 
+    {
+        if((s[i]>='A'&&s[i]<= 'Z')||(s[i]>='a'&&s[i]<='z')||(s[i]>='0'&&s[i]<='9')) 
+            c[x++] = s[i];
+        else 
+        {
+            c[x] = '\0'; 
+            if (strcmp(w, c) == 0) 
+            {
+                k = 1;
+                break;
+            } else 
+            {
+                x = 0;
+            }
+        }
+    }
+    c[x] = '\0'; 
+    if (strcmp(w, c) == 0) 
+        k = 1;
+
+    if (k == 1) 
+        printf("The substring is present.\n");
+     else 
+        printf("The substring is not present.\n");
+}
